bed29715990d1d5ad5b03d0c60894c36534ee870
k2dev404 <dev404@ya.ru>
2016-09-03 08:03:53 +0300

    test22

--- index.php
+++ index.php
@@ -1,12 +1,4 @@
 <?
-/**
-* @copyright (C) 2005-2011 K2CMS
-* @license http://www.gnu.org/licenses/gpl.html GPL version 2 or higher
-*/
-
-//include_once($_SERVER['DOCUMENT_ROOT'].'/k2/content.php');
-
-// test
 
 echo 1;
 ?>
